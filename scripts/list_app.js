2b86258016b6e9cbd2fe86b05f9117ab07a4eebf
Nam Pham <nampham0494@gmail.com>
2020-04-18 18:56:40 +0700

    Update FaceApp and Reddit

--- scripts/list_app.js
+++ scripts/list_app.js
@@ -1,1 +1,13 @@
-list_app.js
+let list = [
+	"com.midnight.snell",
+	"com.ilbsoft.irelax",
+	"com.meitu.vcus",
+	"com.BritishCouncil.playtime",
+	"com.lexwarelabs.goodmorning",
+	"com.ginnypix.kuni",
+	"com.macpaw.iosgemini",
+	"com.stey",
+	"com.midnight.snell",
+	"com.cribster.jsondesigner",
+	"com.appcubby.launchpro"
+];
